7117f3ec635ac6100c5a6f9e6f90ecf4ad45c8ce
zhuyy2013 <zhuyy2013@gmail.com>
2015-02-11 22:32:17 -0500

    update leetcode

---
+++ leetcode/excel_column_number_171.cc
@@ -0,0 +1,11 @@
+//Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int sum = 0;
+        for (int i = 0; i < s.length(); ++i)
+            sum = sum * 26 + s[i] - 'A' + 1;
+        return sum;
+    }
+};
